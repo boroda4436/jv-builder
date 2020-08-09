5603b3219ee5bf3b06d644066186d48df6b50104
Bohdan Chupika <boroda4436@users.noreply.github.com>
2020-08-09 22:47:10 +0300

    Update HelloWorld.java

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -4,5 +4,11 @@ package core.basesyntax;
  * Feel free to remove this class and create your own.
  */
 public class HelloWorld {
-
+      public void push(String value) {
+        int hello = 10;
+        int world = 15;
+        int result = hello + world;
+        result = 10;
+        System.out.println(result);
+    }
 }
